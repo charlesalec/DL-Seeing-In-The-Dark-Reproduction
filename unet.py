8c033bc520ceb765cbd26ff54801dc0051e4df53
Rico van Buren <ricovanburen@gmail.com>
2023-03-30 11:25:28 +0200

    Set loss to MSE and make net the same as paper

--- unet.py
+++ unet.py
@@ -86,17 +86,15 @@ class build_unet(nn.Module):
         self.e2 = encoder_block(32, 64)
         self.e3 = encoder_block(64, 128)
         self.e4 = encoder_block(128, 256)
-        self.e5 = encoder_block(256, 512)
 
         """ Bottleneck """
-        self.b = conv_block(512, 1024) 
+        self.b = conv_block(256, 512)
 
         """ Decoder """
-        self.d1 = decoder_block(1024, 512)
-        self.d2 = decoder_block(512, 256)
-        self.d3 = decoder_block(256, 128)
-        self.d4 = decoder_block(128, 64)
-        self.d5 = decoder_block(64, 32)
+        self.d1 = decoder_block(512, 256)
+        self.d2 = decoder_block(256, 128)
+        self.d3 = decoder_block(128, 64)
+        self.d4 = decoder_block(64, 32)
 
         """ Classifier """
         self.outputs = nn.Conv2d(32, 12, kernel_size=1, padding=0)
@@ -107,20 +105,18 @@ class build_unet(nn.Module):
         s2, p2 = self.e2(p1)
         s3, p3 = self.e3(p2)
         s4, p4 = self.e4(p3)
-        s5, p5 = self.e5(p4)
 
         """ Bottleneck """
-        b = self.b(p5)
+        b = self.b(p4)
 
         """ Decoder """
-        d1 = self.d1(b, s5)
-        d2 = self.d2(d1, s4)
-        d3 = self.d3(d2, s3)
-        d4 = self.d4(d3, s2)
-        d5 = self.d5(d4, s1)
+        d1 = self.d1(b, s4)
+        d2 = self.d2(d1, s3)
+        d3 = self.d3(d2, s2)
+        d4 = self.d4(d3, s1)
 
         """ Classifier """
-        outputs = self.outputs(d5)
+        outputs = self.outputs(d4)
         outputs = nn.functional.pixel_shuffle(outputs, 2)
 
         return outputs
